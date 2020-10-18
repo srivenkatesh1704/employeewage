895a873a20044e7df8b2d57fae0e652e90a84ca3
venkatesh <srivenkatesh17041999@gmail.com>
2020-10-18 14:25:51 +0530

    code for attendance check

--- employee.sh
+++ employee.sh
@@ -1,2 +1,14 @@
 echo "welcome to employee program"
+ispresent=1
+isabsent=0
+empcheck=$((RANDOM%2))
+case  $empcheck in
+      $ispresent)
+          echo  "employee is present"
+          ;;
+      $isabsent)
+          echo "employee is absent"
+          ;;
+      *)
+esac
 

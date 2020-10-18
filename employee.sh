4f783eee104bb388ff5f8f98ce75adf7c2510c1e
venkatesh <srivenkatesh17041999@gmail.com>
2020-10-18 20:20:33 +0530

    total hours code

--- employee.sh
+++ employee.sh
@@ -43,5 +43,43 @@ case $emp in
      *)
 esac
 done
+echo $sum
+emp=$(( RANDOM%3 ))
+emrph=20
+fulltime=1
+overtime=2
+absent=0
+sum=0
+totalhours=0
+for (( i=1; i<=20; i++ ))
+do
+case $emp in
+     $fulltime)
+        if [ $totalhours -le 100 ]
+        then
+        dailywage=$((8*20))
+        sum=$((dailywage + sum))
+        totalhours=$((8+totalhours))
+        else
+        break
+        fi
+        ;;
+     $overtime)
+        if [ $totalhours -le 100 ]
+        then
+        dailywage=$((12*20))
+        sum=$((dailywage+sum))
+        totalhours=$((12+totalhours))
+        else
+        break
+        fi
+        ;;
+     $absent)
+        dailywage=0
+        ;;
+     *)
+esac
+done
+echo $sum
 
 
